1fa6517c3e039bacaff7ef02408bfa9119e34d31
Tharathips <jungjingzaa@gmail.com>
2017-05-03 18:58:55 +0700

    Add files via upload

---
+++ wallet.php
@@ -0,0 +1,355 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>GANGSTER</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="./bootstrap.css" media="screen">
+  <link rel="stylesheet" href="../assets/css/custom.min.css">
+  <link rel="icon" href="wallet.png">
+</head>
+
+<body id="zz" data-spy="scroll" data-target=".navbar" data-offset="50" role="document">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#zz">ALLACC SHOP</a>
+	  </div>
+	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#zz">HOME<span class="sr-only">(current)</span></a></li>
+		
+        <li><a href="twol.php">BACK</a></li>
+	  </div>
+</nav>
+
+<br><br><br><br><center>
+
+<div class="w3-container" id="wallet">
+  <h1>WALLET</h1>
+  <p>100% Merino wool felt, premium leather.</p>
+  </div>
+  
+  <div><img src="allacc/all/n.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('n').style.display='block'">
+
+    <div id="n" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/n.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/all/kkk.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nn').style.display='block'">
+  
+    <div id="nn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/kkk.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/all/mmm.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nnn').style.display='block'">
+  
+    <div id="nnn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/mmm.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br>
+	<div><img src="allacc/mac/cccc.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('n').style.display='block'">
+
+    <div id="n" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/mac/cccc.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/mac/h.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nn').style.display='block'">
+  
+    <div id="nn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/h.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/mac/ff.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nnn').style.display='block'">
+  
+    <div id="nnn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/ff.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br>
+	<div><img src="allacc/mac/c.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('c').style.display='block'">
+
+    <div id="c" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/mac/c.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/mac/cc.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('cc').style.display='block'">
+  
+    <div id="cc" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/cc.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/mac/ccc.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('ccc').style.display='block'">
+  
+    <div id="ccc" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/ccc.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br>
+	<div><img src="allacc/ipad/v.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('l').style.display='block'">
+
+    <div id="l" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/ipad/v.jpg" style="width:40%">
+	  <h4>Surf Wallet</h4><hr>
+	<p>Herschel Supply Hank Bifold Wallet.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/mac/vv.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('ll').style.display='block'">
+  
+    <div id="ll" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/vv.jpg" style="width:40%">
+	  <h4>Herschel Supply Raven Black</h4><hr>
+	<p>Card holder Money clip 269 641.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/mac/dd.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('lll').style.display='block'">
+  
+    <div id="lll" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/dd.jpg" style="width:40%">
+	  <h4>MacBook Sleeve</h4><hr>
+	<p>Touch Bar MacBook Pro sizes available.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br>
+	<div><img src="allacc/mac/w.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('0').style.display='block'">
+
+    <div id="0" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/mac/w.jpg" style="width:40%">
+	  <h4>Walnut Card Wallet </h4><hr>
+	<p>Walnut Bifold Wallet.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/mac/re.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('00').style.display='block'">
+  
+    <div id="00" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/re.jpg" style="width:40%">
+	  <h4>De awesome houten portemonnee van Slim Timber</h4><hr>
+	<p>De awesome houten portemonnee van Slim Timber.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/mac/za.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('000').style.display='block'">
+  
+    <div id="000" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/za.jpg" style="width:40%">
+	  <h4>walnut wood bifold by slim timber</h4><hr>
+	<p>walnut wood bifold by slim timber.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br>
+	<div><img src="allacc/mac/ds.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('1').style.display='block'">
+
+    <div id="1" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/mac/ds.jpg" style="width:40%">
+	  <h4>GUCCI Patent</h4><hr>
+	<p>GUCCI Patent.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/mac/xs.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('11').style.display='block'">
+  
+    <div id="11" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/xs.jpg" style="width:40%">
+	  <h4>GUCCI PatentGucci Signature</h4><hr>
+	<p>Gucci Signature.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/mac/fd.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('111').style.display='block'">
+  
+    <div id="111" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/mac/fd.jpg" style="width:40%">
+	  <h4>Gucci Soho Red Leather Medium</h4><hr>
+	<p>Gucci Soho Red Leather Medium.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div>
+	
+	<br><br><br><br>
+	  
+    <a href="twol.php" class="btn btn-default">BACK</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="#zz" class="btn btn-primary">ON TOP</a>
+	
+	<br><br><br><br></center>
+	
+	<?php
+ error_reporting(0);
+ $appid = "web592group05.appspot.com";
+ $page = $_GET['p'];
+ if($page=='') $page='Comment';
+ $title = $page;
+ function panel_include($title,$file,$ptype='default'){
+echo "<div class='panel panel-$ptype'>";
+echo "<div class='panel-heading'>$title</div>";
+echo "<div class='panel-body'>";
+if(file_exists($file)){
+ include($file);
+}else{
+ echo "ไม่พบไฟล์ $file ";
+}
+echo "</div>";
+echo "</div>";
+ }
+
+use google\appengine\api\cloud_storage\CloudStorageTools;
+function userpic($uid){
+ global $appid;
+ $userpic="gs://$appid/{$uid}.jpg";
+ if(!file_exists($userpic)){
+ return "user.png";
+ }
+ return CloudStorageTools::getImageServingUrl($userpic,["size"=>200]);
+}
+?>
+
+<?php
+ readfile("gs://$appid/header.html");
+?>
+</header>
+<div class="container">
+<div class="col-sm-3">
+<?php panel_include("User","work1_user.php"); ?>
+
+</div>
+
+<div class="col-sm-9">
+<?php panel_include($title,"work1_body.php" ,"primary"); ?>
+</div>
+</div>
+ <footer class="text-center page-footer">
+ <?php
+ readfile("gs://$appid/footer.html");
+ 
+ use google\appengine\api\users\UserService;
+ if (UserService::isCurrentUserAdmin()){
+ echo "<br><a href='?p=edit&file=header.html' class='btn btn-default'>แก้ไข header</a>";
+ echo "<a href='?p=edit&file=footer.html' class='btn btn-default'>แก้ไข footer</a>";
+ }
+?>
+
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#xx']").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+})
+</script>
+</body>
+</html>

cbbe673bb8da5d4ffb8e918e1d4e69b44a713761
koekaew <naruto14789@hotmail.com>
2017-05-03 18:51:24 +0700

    Add files via upload

---
+++ twol.php
@@ -0,0 +1,330 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>GANGSTER</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="./bootstrap.css" media="screen">
+  <link rel="stylesheet" href="../assets/css/custom.min.css">
+  <link rel="icon" href="wallet.png">
+</head>
+
+<body id="zz" data-spy="scroll" data-target=".navbar" data-offset="50" role="document">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#zz">ALLACC SHOP</a>
+	  </div>
+	  
+	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#zz">HOME<span class="sr-only">(current)</span></a></li>
+        <li><a href="#key">KEY</a></li>
+        <li><a href="#case">CASE</a></li>
+		<li><a href="#wallet">WALLET</a></li>
+		<li><a href="#other">OTHER</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<br><br><br><br><center>
+
+<div class="w3-container" id="key">
+  <h1>KEY</h1>
+  <p>Key holder and keychain.</p>
+</div>
+
+<div><img src="allacc/all/ff.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('a').style.display='block'">
+
+ <div id="a" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/ff.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/hh.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('b').style.display='block'">
+  
+  <div id="b" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/hh.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/g.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('c').style.display='block'">
+  
+  <div id="c" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/g.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div><br>
+	  <ul class="pagination pagination-lg">
+      <li><a href="key.php">More &raquo;</a></li>
+      </ul>
+	  
+	<br><br><br><br>
+  <div class="w3-container" id="case">
+  <h1>CASE</h1>
+  <p>MacBook charger, cords, dongles.</p>
+</div>
+
+<div><img src="allacc/all/aa.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('f').style.display='block'">
+
+    <div id="f" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/aa.jpg" style="width:40%">
+	  <h4>Compact Case</h4><hr>
+	<p>Compact Case, the missing accessory for your MacBook or iPad.<br>
+	Holds your Apple charger or small items you carry with you daily.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/all/bb.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('ff').style.display='block'">
+  
+    <div id="ff" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/bb.jpg" style="width:40%">
+	  <h4>Compact Case</h4><hr>
+	<p>Compact Case, the missing accessory for your MacBook or iPad.<br>
+	Holds your Apple charger or small items you carry with you daily.</p> 
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/all/a.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('fff').style.display='block'">
+  
+    <div id="fff" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/a.jpg" style="width:40%">
+	  <h4>Compact Case</h4><hr>
+	<p>Compact Case, the missing accessory for your MacBook or iPad.<br>
+	Holds your Apple charger or small items you carry with you daily.</p> 
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div><br>
+	<ul class="pagination pagination-lg">
+      <li><a href="case.php">More &raquo;</a></li>
+      </ul>	
+
+	  <br><br><br><br>
+  <div class="w3-container" id="wallet">
+  <h1>WALLET</h1>
+  <p>100% Merino wool felt, premium leather.</p>
+</div>
+
+<div><img src="allacc/all/n.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('n').style.display='block'">
+
+    <div id="n" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/n.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/all/kkk.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nn').style.display='block'">
+  
+    <div id="nn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/kkk.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/all/mmm.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('nnn').style.display='block'">
+  
+    <div id="nnn" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/all/mmm.jpg" style="width:40%">
+	  <h4>Card Wallet</h4><hr>
+	<p>Minimal wallet to cover your card carry needs.<br>
+	Two leather compartments separated by a supple layer of wool<br>
+	will safely hold your everyday cards: bank cards, IDs, metro pass.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div><br>
+	<ul class="pagination pagination-lg">
+      <li><a href="wallet.php">More &raquo;</a></li>
+      </ul>
+	  
+	  <br><br><br><br>
+	  <div class="w3-container" id="other">
+  <h1>OTHER</h1>
+  <p>Bra Bra Bra.</p>
+</div>
+
+<div><img src="allacc/other/a.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('q').style.display='block'">
+
+    <div id="q" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/other/a.jpg" style="width:40%">
+	  <h4>Handbags</h4><hr>
+	<p>กระเป๋าแฟชั่น สไตล์เกาหลี สีดำคลาสสิค ทรงหมอนใบกลางตั้งได้ ดีไซน์สวยเรียบหรู ดูดี.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+	<img src="allacc/other/b.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('qq').style.display='block'">
+  
+    <div id="qq" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/other/b.jpg" style="width:40%">
+	  <h4>backpack</h4><hr>
+	<p>กระเป๋าเป้แฟชั่น สีชมพู ขนาดกระทัดรัด เย็บตาราง ดีไซน์สวยเก๋เท่ๆ ไม่ซ้ำใคร.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	
+	<img src="allacc/other/c.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('qqq').style.display='block'">
+  
+    <div id="qqq" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	  <img src="allacc/other/c.jpg" style="width:40%">
+	  <h4>backpack</h4><hr>
+	<p>กระเป๋าเป้แฟชั่น สไตล์เกาหลี สีไวน์แดง  3 in 1 เย็บตารางทั้งใบ ดีไซน์สวยหรูเก๋ๆ.</p>
+	</div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+	</div><br>
+	<ul class="pagination pagination-lg">
+      <li><a href="other.php">More &raquo;</a></li>
+      </ul>
+	  
+</center><br><br>
+
+
+
+<?php
+ error_reporting(0);
+ $appid = "web592group05.appspot.com";
+ $page = $_GET['p'];
+ if($page=='') $page='Comment';
+ $title = $page;
+ function panel_include($title,$file,$ptype='default'){
+echo "<div class='panel panel-$ptype'>";
+echo "<div class='panel-heading'>$title</div>";
+echo "<div class='panel-body'>";
+if(file_exists($file)){
+ include($file);
+}else{
+ echo "ไม่พบไฟล์ $file ";
+}
+echo "</div>";
+echo "</div>";
+ }
+
+use google\appengine\api\cloud_storage\CloudStorageTools;
+function userpic($uid){
+ global $appid;
+ $userpic="gs://$appid/{$uid}.jpg";
+ if(!file_exists($userpic)){
+ return "user.png";
+ }
+ return CloudStorageTools::getImageServingUrl($userpic,["size"=>200]);
+}
+?>
+
+<?php
+ readfile("gs://$appid/header.html");
+?>
+</header>
+<div class="container">
+<div class="col-sm-3">
+<?php panel_include("User","work1_user.php"); ?>
+
+</div>
+
+<div class="col-sm-9">
+<?php panel_include($title,"work1_body.php" ,"primary"); ?>
+</div>
+</div>
+ <footer class="text-center page-footer">
+ <?php
+ readfile("gs://$appid/footer.html");
+ 
+ use google\appengine\api\users\UserService;
+ if (UserService::isCurrentUserAdmin()){
+ echo "<br><a href='?p=edit&file=header.html' class='btn btn-default'>แก้ไข header</a>";
+ echo "<a href='?p=edit&file=footer.html' class='btn btn-default'>แก้ไข footer</a>";
+ }
+?>
+<br><br>
+<a href="index.php" class="btn btn-primary">BACK HOME</a>
+<br><br>
+
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#zz']").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+})
+</script>
+</body>
+</html>

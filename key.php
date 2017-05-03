08e3191d6ab27f606a7ef20bffc00d11b42b5804
Tharathips <jungjingzaa@gmail.com>
2017-05-03 18:57:41 +0700

    Add files via upload

---
+++ key.php
@@ -0,0 +1,360 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>GANGSTER</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="./bootstrap.css" media="screen">
+  <link rel="stylesheet" href="../assets/css/custom.min.css">
+  <link rel="icon" href="wallet.png">
+</head>
+
+<body id="zz" data-spy="scroll" data-target=".navbar" data-offset="50" role="document">
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#zz">ALLACC SHOP</a>
+	  </div>
+	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#zz">HOME<span class="sr-only">(current)</span></a></li>
+		
+        <li><a href="twol.php">BACK</a></li>
+	  </div>
+</nav>
+
+<br><br><br><br><center>
+
+<div class="w3-container">
+  <h1>KEY</h1>
+  <p>Key holder and keychain.</p>
+</div>
+
+<div><img src="allacc/all/ff.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('a').style.display='block'">
+
+ <div id="a" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/ff.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/hh.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('b').style.display='block'">
+  
+  <div id="b" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/hh.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/g.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('c').style.display='block'">
+  
+  <div id="c" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/g.jpg" style="width:40%">
+	  <h4>Key Two</h4><hr>
+	<p>2 in 1 leather key holder with a leather cord keychain loop. <br>
+		Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+	  
+	  <br><br><br>
+<div><img src="allacc/all/ii.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('z').style.display='block'">
+
+ <div id="z" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/ii.jpg" style="width:40%">
+	  <h4>Loop Wallet Case</h4><hr>
+	<p>Casual smartphone case with two card slots and a wrist loop. <br>
+	   Unique locking design provides quick access to phone and </p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/jj.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('zz').style.display='block'">
+  
+  <div id="zz" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/jj.jpg" style="width:40%">
+	  <h4>Loop Wallet Case</h4><hr>
+	<p>Casual smartphone case with two card slots and a wrist loop. <br>
+	   Unique locking design provides quick access to phone and </p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/z.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('zz').style.display='block'">
+  
+  <div id="zz" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/z.jpg" style="width:40%">
+	  <h4>Key holder and keychain</h4><hr>
+	<p> Use together as a set, or if using two sets of keys, pair one with the car key,<br>
+		and the other with your house keys. Or any way you want to play with them.
+		</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+	  
+	  <br><br><br>
+<div><img src="allacc/all/x.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('r').style.display='block'">
+
+ <div id="r" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/x.jpg" style="width:40%">
+	  <h4>Uninnova</h4><hr>
+	<p>Uninnovaรถคลาสสิกกระเป๋าพวงกุญแจ.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/v.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('rr').style.display='block'">
+  
+  <div id="rr" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/v.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>พวงกุญแจรถพวงกุญแจกระเป๋าแฟชั่น.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/vv.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('rrr').style.display='block'">
+  
+  <div id="rrr" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/vv.jpg" style="width:40%">
+	  <h4>YJGJZ HOUSE</h4><hr>
+	<p>กระเป๋าสตางค์กุญแจรถหนัง PU มือซิปกรณีผู้ถือพวงกุญแจใส่เหรียญกระเป๋ากระเป๋า.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+
+<br><br><br>
+<div><img src="allacc/all/q.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('w').style.display='block'">
+
+ <div id="w" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/q.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>กระเป๋าสตางค์กลอนกระเป๋ากุญแจที่ใส่สีแดง.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/qq.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('ww').style.display='block'">
+  
+  <div id="ww" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/qq.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>กระเป๋าสตางค์กุญแจรถพวงกุญแจ.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/qqq.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('www').style.display='block'">
+  
+  <div id="www" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/qqq.jpg" style="width:40%">
+	  <h4>Llaveros Hombre</h4><hr>
+	<p>กระเป๋าสตางค์พวงกุญแจหนัง.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+	  
+	  <br><br><br>
+<div><img src="allacc/all/t.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('l').style.display='block'">
+
+ <div id="l" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/t.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Style: Fashion ; Pattern Type: Solid ; Model .</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/l.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('ll').style.display='block'">
+  
+  <div id="ll" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/l.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Pattern Type: Solid ; Genuine Leather Type: Cow Leather ; Item Length: 12 cm ; Gender: Men.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/ll.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('lll').style.display='block'">
+  
+  <div id="lll" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/ll.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Item Width: 9.5 cm ; Style: Fashion ; Pattern Type: Solid ; Genuine Leather Type: Cow Leather.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+	  
+	  <br><br><br>
+<div><img src="allacc/all/w.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('j').style.display='block'">
+
+ <div id="j" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+      <img src="allacc/all/w.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Material Composition: PU ; Style: Fashion ; Pattern Type: Solid ; Brand Name: Senyk ; Shape: Trunk</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/ww.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('jj').style.display='block'">
+  
+  <div id="jj" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/ww.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Main Material: Split Leather ; Brand Name: baellerry ; Gender: Unisex ; Pattern Type: Solid ; Style: Casual</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  
+<img src="allacc/all/www.jpg" style="width:20%;cursor:zoom-in"
+  onclick="document.getElementById('jjj').style.display='block'">
+  
+  <div id="jjj" class="w3-modal" onclick="this.style.display='none'">
+    <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+    <div class="w3-modal-content w3-animate-zoom">
+	<img src="allacc/all/www.jpg" style="width:40%">
+	  <h4>Key Wallets</h4><hr>
+	<p>Pattern Type: Solid ; Genuine Leather Type: Cow Leather ; Model Number: B1014 ; Style: Casual.</p>  
+	  </div></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	  </div>
+	<br><br><br><br>
+	  
+    <a href="twol.php" class="btn btn-default">BACK</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<a href="#zz" class="btn btn-primary">ON TOP</a>
+	
+	<br><br><br><br></center>
+
+<?php
+ error_reporting(0);
+ $appid = "web592group05.appspot.com";
+ $page = $_GET['p'];
+ if($page=='') $page='Comment';
+ $title = $page;
+ function panel_include($title,$file,$ptype='default'){
+echo "<div class='panel panel-$ptype'>";
+echo "<div class='panel-heading'>$title</div>";
+echo "<div class='panel-body'>";
+if(file_exists($file)){
+ include($file);
+}else{
+ echo "ไม่พบไฟล์ $file ";
+}
+echo "</div>";
+echo "</div>";
+ }
+
+use google\appengine\api\cloud_storage\CloudStorageTools;
+function userpic($uid){
+ global $appid;
+ $userpic="gs://$appid/{$uid}.jpg";
+ if(!file_exists($userpic)){
+ return "user.png";
+ }
+ return CloudStorageTools::getImageServingUrl($userpic,["size"=>200]);
+}
+?>
+
+<?php
+ readfile("gs://$appid/header.html");
+?>
+</header>
+<div class="container">
+<div class="col-sm-3">
+<?php panel_include("User","work1_user.php"); ?>
+
+</div>
+
+<div class="col-sm-9">
+<?php panel_include($title,"work1_body.php" ,"primary"); ?>
+</div>
+</div>
+ <footer class="text-center page-footer">
+ <?php
+ readfile("gs://$appid/footer.html");
+ 
+ use google\appengine\api\users\UserService;
+ if (UserService::isCurrentUserAdmin()){
+ echo "<br><a href='?p=edit&file=header.html' class='btn btn-default'>แก้ไข header</a>";
+ echo "<a href='?p=edit&file=footer.html' class='btn btn-default'>แก้ไข footer</a>";
+ }
+?>
+<script>
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#xx']").on('click', function(event) {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+   
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
+})
+</script>
+</body>
+</html>

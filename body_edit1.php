0b9e46becfa3fd6375e7e21a772c681a2632478d
Tharathips <jungjingzaa@gmail.com>
2017-05-27 22:06:41 +0700

    Add files via upload

---
+++ body_edit1.php
@@ -0,0 +1,36 @@
+﻿<?php
+  use google\appengine\api\users\UserService;
+
+  if(!UserService::isCurrentUserAdmin()){
+     echo "คุณไม่มีสิทธิ์ แก้ไข";
+     return;
+  }
+ // Read File in Cloud Storage
+ // Filename must be gs://xxxxxx.appspot.com/filename
+ $file = $_REQUEST['file'];
+ $filename = "gs://$appid/$file";
+ $filedata = "";
+ if($_POST["file"] && $_POST["filedata"]){
+    file_put_contents($filename,$_POST["filedata"]);
+ }
+ if(file_exists($filename)){
+    $filedata = file_get_contents($filename);
+ }
+?>
+<h1>Edit File</h1>
+<form method="post" action="">
+<div class="form-group">
+ <label for="file">File Name</label>
+ <input class="form-control" type='text' name='file'
+value="<?=$file?>">
+</div>
+<div class="form-group">
+ <label for="filedata">File Data</label>
+ <textarea class="form-control" name='filedata' rows=15><?=
+htmlspecialchars($filedata) ?></textarea>
+</div>
+<button class="btn btn-primary">
+<span class="glyphicon glyphicon-floppy-save"></span>
+Save
+</button>
+</form>
